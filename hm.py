f17b26e81001e503bd5c609872779632e4d26901
marichka201184 <66733626+marichka201184@users.noreply.github.com>
2020-11-30 19:20:36 +0200

    Update hm.py

--- hm.py
+++ hm.py
@@ -1,6 +1,5 @@
-
-#1)Дан лист:
-l = [22, 3, 5,2,8,2,-23,8,23,5]
+# 1)Дан лист:
+l = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
 while True:
     print('1. найти min число в листе')
     print('2. удалить все одинаковые значения')
@@ -9,49 +8,46 @@ while True:
     print('5. выход')
     ch = input('Сделайте свой выбор: ')
 
-    if ch=='1' : #  - найти min число в листе
-      print ("min value element : ", min(l))
-      print (min(l))
+    if ch == '1':  # - найти min число в листе
+        print("min value element : ", min(l))
+        print(min(l))
 
-    elif ch=='2': #  - удалить все одинаковые значения
-      print(set(l))
+    elif ch == '2':  # - удалить все одинаковые значения
+        print(set(l))
 
-    elif ch=='3': #  - заменить каждое четвертое значение на "Х"
-     l1=['X' if i % 4 == 0 else l[i-1] for i in range(1,len(l)+1)]
-     print(l1)
+    elif ch == '3':  # - заменить каждое четвертое значение на "Х"
+        l1 = ['X' if i % 4 == 0 else l[i - 1] for i in range(1, len(l) + 1)]
+        print(l1)
 
-    elif ch=='4':
+    elif ch == '4':
+        avg = sum(l) / len(l)
+        print(avg)
+        min = l[0]
 
-      avg=sum(l)/len(l)
-      min=abs(l[0])-avg
-      k=0
-      i=0
-      for i in l:
-       d=abs(i-avg)
-      if d<=min: k=d
-      else :k=min
-      min=k
-      d1=int(avg-min)
-      print(d1)
+        for i in l:
+           d = abs(i - avg)
+           k = abs(min - avg)
+        if d < k:
+            min = i
+        print(min)
 
-    elif ch=='5':
-     print('END')
-     break
+    elif ch == '5':
+        print('END')
+    break
 
-#2)вывести на экран пустой квадрат из "*" сторона которого указана в переменой:
+# 2)вывести на экран пустой квадрат из "*" сторона которого указана в переменой:
 ######################################
-i=0
+i = 0
 size = 7
 for i in range(size):
-    if i == 0 or i == size-1:
-        sq = ('*'+' ') * size
+    if i == 0 or i == size - 1:
+        sq = ('*' + ' ') * size
     else:
-        sq = '* ' + '  ' * (size-2) + '*'
+        sq = '* ' + '  ' * (size - 2) + '*'
     print(sq)
 
-
-#4) вывести табличку умножения с помощью цикла while
-size=5
+# 4) вывести табличку умножения с помощью цикла while
+size = 5
 for row in range(1, size + 1):
     for column in range(1, size + 1):
         print(row * column, end='\t')
